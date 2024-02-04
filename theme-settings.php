6ed64e5d507be9fbf0c31304b165b23d89e68b9a
jillhandman <handmanj@si.edu>
2024-02-03 20:50:07 -0500

    adjust theme files

--- theme-settings.php
+++ theme-settings.php
@@ -5,6 +5,12 @@ use Drupal\system\Form\ThemeSettingsForm;
 use Drupal\Core\Form;
 
 function handy_form_system_theme_settings_alter(&$form, Drupal\Core\Form\FormStateInterface $form_state){
+  $form['st_settings'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Handy Theme Settings'),
+    '#collapsible' => true,
+    '#collapsed' => true,
+  );
 
   // Menu style options
   $form['st_settings']['tabs']['theme_menu_config'] = array(
@@ -22,6 +28,36 @@ function handy_form_system_theme_settings_alter(&$form, Drupal\Core\Form\FormSta
       'menudefault' => t('White - Default'),
       'menu_light' => t('Light'),
       'menu_dark' => t('Dark')
+    ),
+  );
+
+  // Color options
+  $form['st_settings']['tabs']['theme_color_config'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Color setting'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+  );
+
+  $form['st_settings']['tabs']['theme_color_config']['theme_color'] = array(
+    '#type' => 'select',
+    '#title' => t('Color'),
+    '#default_value' => theme_get_setting('theme_color'),
+    '#options'  => array(
+      'default'           => t('Default'),
+      'aqua'              => t('Aqua'),
+      'fuchsia'             => t('Fuchsia'),
+      'grape'           => t('Grape'),
+      'green'       => t('Green'),
+      'leaf'            => t('Leaf'),
+      'navy'            => t('Navy'),
+      'orange'             => t('Orange'),
+      'pink'           => t('Pink'),
+      'purple'           => t('Purple'),
+      'red'           => t('Red'),
+      'sky'           => t('Sky'),
+      'violet'           => t('Violet'),
+      'yellow'           => t('Yellow')
     ),
   );
 }
